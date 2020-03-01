5033f0a247a729aba79bbde6804c4ab36f4ef83c
Wendel Vanderley da Paz <wvp@cesar-ldap.intranet>
2020-03-01 17:13:43 -0300

    update callback

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,9 @@ export default class App extends Component {
   render() {
     return (
       <div className="app--full-height">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Switch>
-            <Route path="/home" component={Home} />
+            <Route path="/home" component={Home}  />
             <Route path="/callback" component={Callback} />
             <Route path="/auth" component={Auth} />
             <Redirect exact from="/" to="auth" />
