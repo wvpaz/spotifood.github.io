ddc4ff342df4b3da9db00e3fbe9ef1f06880c843
Wendel Vanderley da Paz <wvp@cesar-ldap.intranet>
2020-03-01 16:34:27 -0300

    Updates

---
+++ precache-manifest.f4e72de548529884503153c845e177b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd7ec7a963ecaf8ecbaf42ddd1c785d",
+    "url": "/spotifood.github.io/index.html"
+  },
+  {
+    "revision": "8c05467dcb43ada785ed",
+    "url": "/spotifood.github.io/static/css/main.dc98600b.chunk.css"
+  },
+  {
+    "revision": "b7e3c7dc3cbf2be3bb95",
+    "url": "/spotifood.github.io/static/js/2.e5c73df2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/spotifood.github.io/static/js/2.e5c73df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c05467dcb43ada785ed",
+    "url": "/spotifood.github.io/static/js/main.56d61b63.chunk.js"
+  },
+  {
+    "revision": "3498b5052ccc05ce9b1f",
+    "url": "/spotifood.github.io/static/js/runtime-main.ff26e1b1.js"
+  },
+  {
+    "revision": "c4b734435d5866653730d8e93dfd063e",
+    "url": "/spotifood.github.io/static/media/logo.c4b73443.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27b286221959fd10ed2411143364b80f.js"
+  "/spotifood.github.io/precache-manifest.f4e72de548529884503153c845e177b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotifood.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
