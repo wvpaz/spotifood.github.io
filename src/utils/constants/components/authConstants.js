049dd821e6485329d9654f592e81031af70458de
Wendel Vanderley da Paz <wvp@cesar-ldap.intranet>
2020-03-01 17:52:00 -0300

    update redirect

--- src/utils/constants/components/authConstants.js
+++ src/utils/constants/components/authConstants.js
@@ -1,6 +1,6 @@
 export const AUTHORIZATION_SPOTIFY_ENDPOINT = 'https://accounts.spotify.com/authorize';
 export const AUTHORIZATION_SPOTIFY_CLIENT_ID = '05505a02959540328c55a270621221c3';
 export const AUTHORIZATION_SPOTIFY_RESPONSE_TYPE = 'token';
-export const AUTHORIZATION_SPOTIFY_REDIRECT_URI = 'http://localhost:3000/callback';
+export const AUTHORIZATION_SPOTIFY_REDIRECT_URI = 'https://wvpaz.github.io/spotifood.github.io/callback';
 
 export const AUTHORIZATION_LOADING_TEXT = 'Getting Access ...';
